6bdb0e1a5fe44fcc9e4937df4804769beeecd636
aneftz <aivaneftz@gmail.com>
2023-02-11 23:31:38 +0100

    Update js

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ $('.burger_menu').click(function(event) {
     $('body').toggleClass('lock');
 })
 });
+
+$('.da_section,.services,.works,.about,.testimonials,.news,.enquiry,.footer').click((event)=>{
+    if ($('.burger_menu')[0].classList.contains('active'))
+    {
+        let node = event.target;
+        while (node.parentNode)
+        {
+            if (node.classList.contains('menu'))
+                return;
+            node = node.parentNode;
+        }
+        
+        $('.burger_menu,.menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    }
+    console.log('kek');
+})
